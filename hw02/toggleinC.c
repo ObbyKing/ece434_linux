6176bf367ce95ab5e073bd40d8a061826360f126
lundaj <lundaj@rose-hulman.edu>
2018-09-09 19:56:29 -0400

    Making toggle in C

---
+++ hw02/toggleinC.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#include <libsoc_gpio.h>
+#include <libsoc_debug.h>
+
+//Blinks an LED attached to P9_12
+#define GPIO_OUTPUT	60
+
+int main(void) {
+	gpio *gpio_output; // Create gpio pointer
+	libsoc_set_debug(1); // Enable debug output
+	// Request gpio
+	gpio_output = libsoc_gpio_request(GPIO_OUTPUT, LS_SHARED);
+
+	// Set direction to OUTPUT
+	libsoc_gpio_set_direction(gpio_output, OUTPUT);
+
+	libsoc_set_debug(0);
+
+	int i;
+	for(i = 0; i < 1000000; i++){
+		libsoc_gpio_set_level(gpio_output, HIGH);
+		usleep(100000);
+		libsoc_gpio_set_level(gpio_output, LOW);
+		usleep(100000);
+	}
+
+	if (gpio_output){
+		// Free gpio request memory
+		libsic_gpio_free(gpio_output);
+	}
+
+	return EXIT_SUCCESS;
+}
