4b0e7502ecc5ae772e23c222d440d074c8667a6c
ObbyKing <dreweylund@comcast.net>
2018-09-12 00:28:12 +0000

    working on adding gpio

--- hw02/etch-a-sketch.py
+++ hw02/etch-a-sketch.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
+import Adafruit_BBIO.GPIO as GPIO
 import curses
 import sys
 
+#setup the pins
+button0	= "P9_27"
+button1 = "P9_16"
+button2 = "P9_17"
+button3 = "P9_18"
+
+# Set the GPIO pins:
+GPIO.setup(button0, GPIO.IN)
+GPIO.setup(button1, GPIO.IN)
+GPIO.setup(button2, GPIO.IN)
+GPIO.setup(button3, GPIO.IN)
 
 class Sketch(object):
     def __init__(self):
@@ -30,26 +42,34 @@ class Sketch(object):
         except curses.error:
             pass
 
+    def moveCursorUp(self):
+	if self.pos[1] > 0:
+		self.pos[1] -= 1
+
+    def moveCursorDown(self):
+	if self.pos[1] < self.height - 1:
+		self.pos[1] += 1
+
+    def moveCursorLeft(self):
+        if self.pos[0] > 0:
+		self.pos[0] -= 1
+
+    def moveCursorRight(self):
+	if self.pos[0] < self.width - 1:
+		self.pos[0] += 1
+
     def main(self, stdscr):
+	GPIO.add_event_detect(button0, GPIO.HIGH, callback = moveCursorUp)
+	GPIO.add_event_detect(button1, GPIO.HIGH, callback = moveCursorDown)
+	GPIO.add_event_detect(button2, GPIO.HIGH, callback = moveCursorLeft)
+	GPIO.add_event_detect(button3, GPIO.HIGH, callback = moveCursorRight)
         self.stdscr = stdscr
         stdscr.clear()
         self.shake()
         while True:
             self.cursor()
             c = stdscr.getch(self.height - 1, self.width - 1)
-            if c == curses.KEY_UP:
-                if self.pos[1] > 0:
-                    self.pos[1] -= 1
-            elif c == curses.KEY_DOWN:
-                if self.pos[1] < self.height - 1:
-                    self.pos[1] += 1
-            elif c == curses.KEY_LEFT:
-                if self.pos[0] > 0:
-                    self.pos[0] -= 1
-            elif c == curses.KEY_RIGHT:
-                if self.pos[0] < self.width - 1:
-                    self.pos[0] += 1
-            elif c == ord('q'):
+            if c == ord('q'):
                 break
             elif c == ord('s'):
                 self.shake()
