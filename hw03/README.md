181848b52870e7d774b2cc0616dd6e30a7f012a5
ObbyKing <dreweylund@comcast.net>
2018-09-14 19:16:18 +0000

    finishing homework 3

---
+++ hw03/README.md
@@ -0,0 +1,16 @@
+I started with the temperature sensors. I have one temp sensor tied to ground and one tied to high.
+In my script, I read both of the addresses and convert the value to Fahrenheit. This worked correctly.
+
+I then moved onto etch-a-sketch. I got the LED matrix working with the buttons first before the rotary encoders.
+The buttons successfully worked and then I moved onto the encoders.
+
+Run the following commands to get the encoders to work properly
+config-pin P8_33 qep
+config-pin P8_35 qep
+
+config-pin P8_41 qep
+config-pin P8_42 qep
+
+I ran etch-a-sketch.py and demoed it to Dr. Yoder. It worked correctly.
+
+I also added a clear button and a quit button to my project.
--- hw03/etch-a-sketch.py
+++ hw03/etch-a-sketch.py
@@ -4,8 +4,9 @@ import curses
 import sys
 import time
 import smbus
+from Adafruit_BBIO.Encoder import RotaryEncoder, eQEP1, eQEP2
 
-delay = 1;
+delay = 2
 
 class Sketch(object):
     def __init__(self):
@@ -19,6 +20,9 @@ class Sketch(object):
         self.empty = " "
         self.cursorc = "O"
         self.stdscr = None
+        self.ledState = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+        self.matrix = 0x70
+        self.bus = smbus.SMBus(2)
 
     # Shake gets rid of everything on the screen
     def shake(self):
@@ -29,12 +33,25 @@ class Sketch(object):
                     self.stdscr.addstr(y, x, self.empty)
                 except curses.error:
                     pass
+        for x in range (0, len(self.ledState)):
+                self.ledState[x] = 0x00
+        self.bus.write_i2c_block_data(self.matrix, 0, self.ledState)
     # This is the cursor function which writes to a position
     def cursor(self):
         try:
             self.stdscr.addstr(self.pos[1], self.pos[0], self.cursorc)
+            currentStateRed = self.ledState[(self.pos[0]*2)+1]
+            self.ledState[(self.pos[0]*2)+1] = currentStateRed | (1 << self.pos[1])
+            self.bus.write_i2c_block_data(self.matrix, 0, self.ledState)
         except curses.error:
             pass
+
+    def updateLEDMatrix(self):
+        currentStateGreen = self.ledState[(self.oldpos[0]*2)]
+        self.ledState[(self.oldpos[0]*2)] = currentStateGreen | (1 << self.oldpos[1])
+        currentStateRed = self.ledState[(self.oldpos[0]*2)+1]
+        self.ledState[(self.oldpos[0]*2)+1] = 0x00
+        self.bus.write_i2c_block_data(self.matrix, 0, self.ledState)
 
     def main(self, stdscr):
         # Defining Buttons
@@ -51,12 +68,17 @@ class Sketch(object):
         GPIO.setup(buttonRight, GPIO.IN)
         GPIO.setup(buttonQuit, GPIO.IN)
         GPIO.setup(buttonShake, GPIO.IN)
+        # Setup the encoders
+        upDownEncoder = RotaryEncoder(eQEP2)
+        leftRightEncoder = RotaryEncoder(eQEP1)
+        upDownEncoder.setAbsolute()
+        leftRightEncoder.setAbsolute()
+        upDownEncoder.enable()
+        leftRightEncoder.enable()
         # Setting up the LED matrix
-        bus = smbus.SMBus(2)  # Use i2c bus 1
-        matrix = 0x70         # Use address 0x70
-        bus.write_byte_data(matrix, 0x21, 0)   # Start oscillator (p10)
-        bus.write_byte_data(matrix, 0x81, 0)   # Disp on, blink off (p11)
-        bus.write_byte_data(matrix, 0xe7, 0)   # Full brightness (page 15)
+        self.bus.write_byte_data(self.matrix, 0x21, 0)   # Start oscillator (p10)
+        self.bus.write_byte_data(self.matrix, 0x81, 0)   # Disp on, blink off (p11)
+        self.bus.write_byte_data(self.matrix, 0xe7, 0)   # Full brightness (page 15)
         # Pointing the screen to itself
         self.stdscr = stdscr
         stdscr.clear()
@@ -67,16 +89,16 @@ class Sketch(object):
                 self.cursor()
                 # Initliaze the screen
                 curses.initscr()
-                if GPIO.input(buttonUp):
+                if GPIO.input(buttonUp) | upDownEncoder.position > 0:
                     if self.pos[1] > 0:
                         self.pos[1] -= 1
-                elif GPIO.input(buttonDown):
+                elif GPIO.input(buttonDown) | upDownEncoder.position < 0:
                     if self.pos[1] < self.height - 1:
                         self.pos[1] += 1
-                elif GPIO.input(buttonLeft):
+                elif GPIO.input(buttonLeft) | leftRightEncoder.position > 0:
                     if self.pos[0] > 0:
                         self.pos[0] -= 1
-                elif GPIO.input(buttonRight):
+                elif GPIO.input(buttonRight) | leftRightEncoder.position < 0:
                     if self.pos[0] < self.width - 1:
                         self.pos[0] += 1
                 elif GPIO.input(buttonQuit):
@@ -87,8 +109,11 @@ class Sketch(object):
                     continue
                 else:
                     continue
+                upDownEncoder.position = 0
+                leftRightEncoder.position = 0
                 # Draw something on the board
                 stdscr.addstr(self.oldpos[1], self.oldpos[0], self.full)
+                self.updateLEDMatrix()
                 # Update the old position
                 self.oldpos = list(self.pos)
                 time.sleep(delay/10)
